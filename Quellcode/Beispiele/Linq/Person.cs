5e90b85212a1d1364cf047f4df874e76a76511f1
michaelspeer <michael.speer@generic.de>
2015-05-06 09:38:20 +0200

    Aufräumen

--- Quellcode/Beispiele/Linq/Person.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DHBW.EinstiegDotNet.Beispiele.Linq
-{
-    public class Person
-    {
-        public string Vorname { get; set; }
-        public string Nachname { get; set; }
-        public int Alter { get; set; }
-    }
-}
--- Quellcode/Beispiele/Syntax/Unicode.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DHBW.EinstiegDotNet.Beispiele.Syntax
-{
-    public class Unicode
-    {
-        public int ÄußereQualität { get; set; }
-        public string Département { get; set; }
-
-    }
-}
